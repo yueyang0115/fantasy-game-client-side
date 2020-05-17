87e83e8674cfef047dd1be5739ca0aef760d75e9
Yunzhe Jia <yj@MBP-Yunzhe.local>
2020-05-17 14:44:20 -0400

    rewrite some part of communicator

--- FantasyClient/app/src/main/java/com/example/fantasyclient/communicator.java
+++ FantasyClient/app/src/main/java/com/example/fantasyclient/communicator.java
@@ -1,5 +1,7 @@
 package com.example.fantasyclient;
 
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -11,22 +13,38 @@ import java.net.Socket;
 
 public class communicator {
     private Socket socket;
+    private BufferedWriter bw;
+    private BufferedReader br;
+    private static final String TAG = "communicator";
 
     public communicator(Socket in) {
         socket = in;
+        try{
+            bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }catch (IOException e){
+            Log.e(TAG,"Failed to construct communicator!");
+        }
+
     }
 
-    public void send_msg(String msg) throws IOException {
-        OutputStream os = socket.getOutputStream();
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
-        bw.write(msg);
-        bw.flush();
+    public void send_msg(String msg) {
+        try{
+            bw.write(msg);
+            bw.flush();
+        } catch (IOException e){
+            Log.e(TAG,"Failed to send data!");
+        }
+
     }
 
-    public String recv_msg() throws IOException {
-        InputStream is = socket.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        String msg=br.readLine();
+    public String recv_msg()  {
+        String msg="";
+        try{
+            msg=br.readLine();
+        } catch(IOException e){
+            Log.e(TAG,"Failed to receive data!");
+        }
         return msg;
     }
 }
