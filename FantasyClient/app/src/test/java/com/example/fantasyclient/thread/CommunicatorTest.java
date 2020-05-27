f54da00b5c1743e6769756a42e860d22f7e428b4
hm171 <hm171@duke.edu>
2020-05-27 18:27:23 -0400

    hm:test_communicator

---
+++ FantasyClient/app/src/test/java/com/example/fantasyclient/thread/CommunicatorTest.java
@@ -0,0 +1,20 @@
+package com.example.fantasyclient.thread;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommunicatorTest {
+
+    @Test
+    public void test_Communicator(){
+        sendMsg();
+        recvMsg();
+    }
+
+    public void sendMsg() {
+    }
+
+    public void recvMsg() {
+    }
+}
