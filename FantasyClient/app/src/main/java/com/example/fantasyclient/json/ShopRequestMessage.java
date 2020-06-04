02b8325a92b442f622d5cb124e0b176a0d79ae95
Yunzhe Jia <yj@MBP-Yunzhe.local>
2020-06-04 18:47:46 -0400

    add itemid to shopRequest

--- FantasyClient/app/src/main/java/com/example/fantasyclient/json/ShopRequestMessage.java
+++ FantasyClient/app/src/main/java/com/example/fantasyclient/json/ShopRequestMessage.java
@@ -3,6 +3,7 @@ package com.example.fantasyclient.json;
 public class ShopRequestMessage {
     private int shopID;
     private int territoryID;
+    private int itemID;
     private String action;//"list""buy""sell"
 
     public ShopRequestMessage() {
