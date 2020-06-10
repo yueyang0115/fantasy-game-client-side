39a5569cc20529b2bfb7c7d14dd0faa2b8be6127
hm171 <hm171@duke.edu>
2020-06-10 17:15:17 -0400

    hm: PositionRequestMessage

--- FantasyClient/app/src/main/java/com/example/fantasyclient/json/PositionRequestMessage.java
+++ FantasyClient/app/src/main/java/com/example/fantasyclient/json/PositionRequestMessage.java
@@ -1,29 +1,23 @@
 package com.example.fantasyclient.json;
 
-public class PositionRequestMessage {
-    private int x;
-    private int y;
+import com.example.fantasyclient.model.WorldCoord;
 
-    public PositionRequestMessage(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+import java.util.List;
+
+public class PositionRequestMessage {
+    private List<WorldCoord> coords;
 
     public PositionRequestMessage(){}
 
-    public int getX() {
-        return x;
+    public PositionRequestMessage(List<WorldCoord> coords) {
+        this.coords = coords;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public List<WorldCoord> getCoords() {
+        return coords;
     }
 
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
+    public void setCoords(List<WorldCoord> coords) {
+        this.coords = coords;
     }
 }
