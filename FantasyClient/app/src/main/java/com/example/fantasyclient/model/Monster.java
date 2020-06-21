5e553a556061afe0190d4dbd140d15bbe110dadd
hm171 <hm171@duke.edu>
2020-06-21 16:14:34 -0400

    hm: add needUpdate to monster

--- FantasyClient/app/src/main/java/com/example/fantasyclient/model/Monster.java
+++ FantasyClient/app/src/main/java/com/example/fantasyclient/model/Monster.java
@@ -4,12 +4,22 @@ public class Monster extends Unit{
 
     private WorldCoord coord;
 
+    private boolean needUpdate;
+
     public WorldCoord getCoord() {
         return coord;
     }
 
     public void setCoord(WorldCoord coord) {
         this.coord = coord;
+    }
+
+    public boolean isNeedUpdate() {
+        return needUpdate;
+    }
+
+    public void setNeedUpdate(boolean needUpdate) {
+        this.needUpdate = needUpdate;
     }
 }
 
